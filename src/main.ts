8e8045c31d9358373a32276bfdf9641ca95c5166
JadlionHD <48410066+JadlionHD@users.noreply.github.com>
2024-02-21 05:51:02 +0700

    feat more tech stacks

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,104 @@ import "./style.css";
 import anime from "animejs";
 import $ from "jquery";
 
-// import typescriptLogo from "./typescript.svg";
-// import viteLogo from "/vite.svg";
-
 function getElById<T>(id: string): T {
   return document.getElementById(id) as T;
 }
 
+window.addEventListener("DOMContentLoaded", () => {
+  document.body.style.display = "block";
+});
+
 let loading = true;
 const main = getElById<HTMLElement>("main");
 const profile = getElById<HTMLElement>("profile");
-// const boxGroup = getElById<HTMLElement>("box-group");
+
+const randomLoading = [
+  `
+  <span>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+    class="m-auto"
+    width="32"
+    height="32"
+    viewBox="0 0 100 100"
+    preserveAspectRatio="xMidYMid"
+  >
+    <g transform="translate(50 50)">
+      <g ng-attr-transform="scale(1)">
+        <g transform="translate(-50 -50)">
+          <path
+            fill="#ffffff"
+            stroke="#ffffff"
+            stroke-width="0"
+            d="M50,14c19.85,0,36,16.15,36,36S69.85,86,50,86S14,69.85,14,50S30.15,14,50,14 M50,10c-22.091,0-40,17.909-40,40 s17.909,40,40,40s40-17.909,40-40S72.091,10,50,10L50,10z"
+          ></path>
+          <path
+            fill="#ffffff"
+            d="M52.78,42.506c-0.247-0.092-0.415-0.329-0.428-0.603L52.269,40l-0.931-21.225C51.304,18.06,50.716,17.5,50,17.5 s-1.303,0.56-1.338,1.277L47.731,40l-0.083,1.901c-0.013,0.276-0.181,0.513-0.428,0.604c-0.075,0.028-0.146,0.063-0.22,0.093V44h6 v-1.392C52.925,42.577,52.857,42.535,52.78,42.506z"
+          >
+            <animateTransform
+              attributeName="transform"
+              type="rotate"
+              repeatCount="indefinite"
+              values="0 50 50;360 50 50"
+              keyTimes="0;1"
+              dur="1s"
+            ></animateTransform>
+          </path>
+          <path
+            fill="#ffffff"
+            d="M58.001,48.362c-0.634-3.244-3.251-5.812-6.514-6.391c-3.846-0.681-7.565,1.35-9.034,4.941 c-0.176,0.432-0.564,0.717-1.013,0.744l-15.149,0.97c-0.72,0.043-1.285,0.642-1.285,1.383c0,0.722,0.564,1.321,1.283,1.363 l15.153,0.971c0.447,0.027,0.834,0.312,1.011,0.744c1.261,3.081,4.223,5.073,7.547,5.073c2.447,0,4.744-1.084,6.301-2.975 C57.858,53.296,58.478,50.808,58.001,48.362z M50,53.06c-1.688,0-3.06-1.373-3.06-3.06s1.373-3.06,3.06-3.06s3.06,1.373,3.06,3.06 S51.688,53.06,50,53.06z"
+          >
+            <animateTransform
+              attributeName="transform"
+              type="rotate"
+              repeatCount="indefinite"
+              values="0 50 50;360 50 50"
+              keyTimes="0;1"
+              dur="4s"
+            ></animateTransform>
+          </path>
+        </g>
+      </g>
+    </g>
+  </svg>
+</span>
+The clock is ticking, please take a moment
+`,
+  `
+  <span>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="32"
+    height="32"
+    fill="currentColor"
+    class="m-auto"
+    viewBox="0 0 16 16"
+  >
+    <path d="M9.167 4.5a1.167 1.167 0 1 1-2.334 0 1.167 1.167 0 0 1 2.334 0" />
+    <path
+      d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0M1 8a7 7 0 0 1 7-7 3.5 3.5 0 1 1 0 7 3.5 3.5 0 1 0 0 7 7 7 0 0 1-7-7m7 4.667a1.167 1.167 0 1 1 0-2.334 1.167 1.167 0 0 1 0 2.334"
+    />
+  </svg>
+</span>
+Girls are now praying, please wait warmly...
+`
+];
+const randLoadingNum = Math.floor(Math.random() * randomLoading.length);
+
+$(getElById("loading-footer")).append(randomLoading[randLoadingNum]);
+
+if (randLoadingNum === 1) {
+  anime({
+    targets: "#loading-footer span svg",
+    loop: true,
+    rotate: "1turn",
+    easing: "linear",
+    duration: 990
+  });
+}
 
 anime({
   targets: "#preloader",
@@ -130,9 +217,7 @@ setTimeout(() => {
 let clicked = false;
 let type = "none";
 
-getElById<HTMLLIElement>("show-skills").addEventListener("click", () => {
-  console.log("ahaah");
-  console.log({ loading, clicked });
+getElById<HTMLLIElement>("show-techs").addEventListener("click", () => {
   if (loading === false) {
     loading = true;
 
@@ -144,7 +229,26 @@ getElById<HTMLLIElement>("show-skills").addEventListener("click", () => {
           duration: 500,
           bottom: "100px"
         }
-      ]
+      ],
+      changeComplete: function () {
+        $(getElById("techs-languages")).fadeIn();
+        anime({
+          targets: "#techs-languages .items",
+          duration: 3000,
+          delay: anime.stagger(100),
+          keyframes: [
+            {
+              duration: 1100,
+              scale: 1.3,
+              opacity: 1
+            },
+            {
+              duration: 2500,
+              scale: 1
+            }
+          ]
+        });
+      }
     });
 
     loading = false;
@@ -171,6 +275,7 @@ closeBtn.addEventListener("click", () => {
   switch (type) {
     case "socials": {
       $(getElById("socials")).fadeOut().fadeTo(500, 0);
+      $(getElById("socials-footer")).fadeOut();
       type = "none";
       break;
     }
@@ -180,6 +285,7 @@ closeBtn.addEventListener("click", () => {
     }
     case "skills": {
       $(getElById("skills")).fadeOut().fadeTo(500, 0);
+      $(getElById("techs-languages")).fadeOut();
       anime({
         targets: "#skills-tabs",
         delay: 500,
@@ -188,7 +294,14 @@ closeBtn.addEventListener("click", () => {
             duration: 500,
             bottom: 0
           }
-        ]
+        ],
+        changeComplete: function () {
+          anime({
+            targets: "#techs-languages .items",
+            delay: anime.stagger(100),
+            opacity: 0
+          });
+        }
       });
 
       type = "none";
@@ -249,25 +362,27 @@ getElById<HTMLLinkElement>("link-socials").addEventListener("click", () => {
 
     hideMain();
     $(getElById("socials")).fadeIn().fadeTo(500, 1);
+    $(getElById("socials-footer")).delay(5000).fadeIn(1000);
     type = "socials";
   }
 });
 
 function showMain() {
-  $(main).fadeIn(500);
-  $(profile).fadeIn(500);
   $(closeBtn).fadeOut(500);
 
   anime({
     targets: "#profile",
-    duration: anime.stagger(100),
+    duration: 200,
     easing: "cubicBezier(0.500, 0.070, 0.000, 0.770)",
     keyframes: [
       {
-        duration: 1000,
         opacity: 1
       }
-    ]
+    ],
+    changeComplete: function () {
+      $(main).fadeIn(500);
+      $(profile).fadeIn(500);
+    }
   });
 
   anime({
